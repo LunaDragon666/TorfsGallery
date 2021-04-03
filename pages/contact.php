93b38a48e2180bdaeac823b1e716a99e220c707b
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-04-03 02:29:53 +0200

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,23 +1,5 @@
 <?php
-  // The global $_POST variable allows you to access the data sent with the POST method by name
-  // To access the data sent with the GET method, you can use $_GET
-  $say = htmlspecialchars($_POST['say']);
-  $to  = htmlspecialchars($_POST['to']);
 
-  echo  $say, ' ', $to;
 
-  from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def form():
-    return render_template('contact.html')
-
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
-    return render_template('index.html', say=request.form['say'], to=request.form['to'])
-
-if __name__ == "__main__":
-    app.run()
 ?>
