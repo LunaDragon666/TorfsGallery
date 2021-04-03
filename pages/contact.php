35e068ef1470c847a141c0aaf83e3ce73fd8c8a2
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-04-03 02:24:36 +0200

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -5,4 +5,19 @@
   $to  = htmlspecialchars($_POST['to']);
 
   echo  $say, ' ', $to;
+
+  from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    return render_template('contact.html')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return render_template('index.html', say=request.form['say'], to=request.form['to'])
+
+if __name__ == "__main__":
+    app.run()
 ?>
