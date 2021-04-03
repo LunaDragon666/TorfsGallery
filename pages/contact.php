7e2be0d546e8d50d0e47f4daa94a55bc66f51548
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-04-03 02:22:27 +0200

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,6 +1,8 @@
 <?php
-if($_POST["Message"]) {
-mail("mmrozec@yahoo.com", "Here is the sample subject line",
-$_POST["Insert Your Message"]. "From: mmrozec@yahoo.com");
-}
+  // The global $_POST variable allows you to access the data sent with the POST method by name
+  // To access the data sent with the GET method, you can use $_GET
+  $say = htmlspecialchars($_POST['say']);
+  $to  = htmlspecialchars($_POST['to']);
+
+  echo  $say, ' ', $to;
 ?>
