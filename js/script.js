800fd6e99be876972b08af361bb3e8771d4f337f
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-05 19:20:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,55 +6,14 @@ navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
 
+// Disable copying images
+function nocontext(e) {
+  var clickedTag = (e==null) ? event.srcElement.tagName : e.target.tagName;
+  if (clickedTag == "IMG")
+    return false;
+}
+
+document.oncontextmenu = nocontext;
+
 
 // GALLERY PAGE SECTION
-  
-const form = document.querySelector("form");
-const firstName = document.querySelector("#firstName");
-const lastName = document.querySelector("#lastName");
-const email = document.querySelector("#email");
-const message = document.querySelector("#message");
-const button = document.querySelector("button");
-
-function checkIfButtonIsDisabled() {
-    // if all inputs pass validation enable the button
-    if (checkLength(firstName.value, 1) && checkLength(lastName.value, 24) && validateEmail(email.value)) {
-        button.disabled = false;
-    } else {
-        // clear the message
-        message.innerHTML = "";
-        // disable button
-        button.disabled = true;
-    }
-}
-
-// call the same function for each input's keyup event
-firstName.addEventListener("keyup", checkIfButtonIsDisabled);
-lastName.addEventListener("keyup", checkIfButtonIsDisabled);
-email.addEventListener("keyup", checkIfButtonIsDisabled);
-
-// function to run when the form is submitted
-function submitForm(event) {
-    event.preventDefault();
-    // display a message once the form has been submitted
-    message.innerHTML = '<div class="message">Your message has been sent</div>';
-    // clear all input values
-    form.reset();
-}
-
-form.addEventListener("submit", submitForm);
-
-// function to check if the length of the input value is valid
-function checkLength(value, len) {
-    if (value.trim().length >= len) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function validateEmail(email) {
-    const regEx = /\S+@\S+\.\S+/;
-    const patternMatches = regEx.test(email);
-    return patternMatches;
-}
