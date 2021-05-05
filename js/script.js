90f010cf54a16cab806c91897bc16c309a8ccbdf
LunaDragon666 <74667575+LunaDragon666@users.noreply.github.com>
2021-05-06 00:43:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,18 @@ function nocontext(e) {
 document.oncontextmenu = nocontext;
 
 // Gallery links
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
